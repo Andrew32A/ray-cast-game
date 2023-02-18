19498dfbb06f5e20763c29e83f869a18bd006d4a
Andrew Alsing <andrew32alsing@gmail.com>
2023-02-17 23:12:09 -0800

    fixed start link by adding a 0.5 sec delayed reload

--- src/App.js
+++ src/App.js
@@ -3,24 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Link } from 'react-router-dom';
 
-function App() {
+function loadGame() {
+  setTimeout(() => {
+    window.location.reload();
+  }, 500);
+}
+
+function App() {  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <Router>
-          <Link to="/game.html">Go to Page 2</Link>
+          <Link to="/game.html" onClick={loadGame}
+          >Start</Link>
         </Router>
       </header>
     </div>
