ea6637946881b3aba53265665c9959bef395c859
Andrew Alsing <andrew32alsing@gmail.com>
2023-02-24 00:02:02 -0800

    THE ENEMY IS RENDERED WITH RAYCASTING

--- src/components/App.js
+++ src/components/App.js
@@ -16,9 +16,9 @@ function App() {
           <Title />
           <ul>
             {/* directly linked the game, BE SURE TO CHANGE IF SITE IS MOVED */}
-            <li><Link to="https://andrew32a.github.io/ray-cast-game/game.html" className='startButton' onClick={loadGame}>Start</Link></li>
-            <li><Link to="Sandbox">Sandbox (not yet linked)</Link></li>
-            <li><Link to="HowToPlay">How to Play</Link></li>
+            <li><Link to="/game.html" className='startButton' onClick={loadGame}>Start</Link></li>
+            <li><Link to="/Sandbox">Sandbox (not yet linked)</Link></li>
+            <li><Link to="/HowToPlay">How to Play</Link></li>
             <li><Link to="https://github.com/Andrew32A/ray-cast-game">Repo</Link></li>
           </ul>
       </header>
