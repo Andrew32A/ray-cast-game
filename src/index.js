85e79f0ab0178b084c6be8e2a6ca4f76076f7458
Andrew Alsing <andrew32alsing@gmail.com>
2023-02-24 15:19:47 -0800

    changed back to hashrouter

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './components/App';
 import HowToPlay from './components/HowToPlay/HowToPlay';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
