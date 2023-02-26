c0c4e59dd46b7b9450517098b1c50ba4c6140f26
Andrew Alsing <andrew32alsing@gmail.com>
2023-02-25 22:51:00 -0800

    added HowToPlay content and styling

--- src/components/HowToPlay/HowToPlay.js
+++ src/components/HowToPlay/HowToPlay.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import "./HowToPlay.css"
-import App from '../App';
+import Title from '../Title/Title';
 
 function HowToPlay() {
     return (
         <div className="HowToPlay">
-            {/* <App /> */}
-            {/* <section> */}
-                {/* <div className="content"> */}
-                    <div className='howto'>press buttons, game go brr</div>
-                {/* </div>
-            </section> */}
+            <Title />
+            <section>
+                <div className="rules">
+                    <div className='rules-title'>How To Play</div>
+                    <div className='rules-content'>
+                        <ol>
+                            <li>If the enemy touches the player, it’s <b className='bolded-red'>game over</b></li>
+                            <li>The player can <b className='bolded-blue'>“freeze”</b> the <b className='bolded-red'>enemy</b> by looking at them</li>
+                            <li>Gain points by looking away from the <b className='bolded-red'>enemy</b></li>
+                        </ol>
+                    </div>
+                </div>
+                <div className="controls">
+                    <div className='controls-title'>Controls</div>
+                    <div className='controls-content'>
+                        <ul>
+                            <li>WASD - Move</li>
+                            <li>Mouse - Camera angle</li>
+                            <li>Q - Toggle minimap</li>
+                        </ul>
+                    </div>
+
+                </div>
+            </section>
         </div>
     )
 }
