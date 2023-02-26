7097477b19dfc2207a5bf609e9da75253643d1c8
Andrew Alsing <andrew32alsing@gmail.com>
2023-02-25 23:04:37 -0800

    added sandbox level

--- src/components/App.js
+++ src/components/App.js
@@ -9,6 +9,12 @@ function loadGame() {
   }, 500);
 }
 
+function loadSandbox() {
+  setTimeout(() => {
+    window.location = "/ray-cast-game/sandbox.html";
+  }, 500);
+}
+
 function App() {  
   return (
     <div className="menuWrapper">
@@ -17,7 +23,7 @@ function App() {
           <ul>
             {/* directly linked the game, BE SURE TO CHANGE IF SITE IS MOVED */}
             <li><div className='btn-one'><p className='startButton option' onClick={loadGame}>Start</p></div></li>
-            <li><div className='btn-one'><Link to="/Sandbox" className='option'>Sandbox (not yet linked)</Link></div></li>
+            <li><div className='btn-one'><p className='startButton option' onClick={loadSandbox}>Sandbox</p></div></li>
             <li><div className='btn-one'><Link to="/HowToPlay" className='option'>How to Play</Link></div></li>
             <li><div className='btn-one'><Link to="https://github.com/Andrew32A/ray-cast-game" className='option'>Repo</Link></div></li>
           </ul>
